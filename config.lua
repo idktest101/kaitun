eb53225bb791c31e268c5a5fc6a42c5d53a0d6d3
idktest101 <usmonbozorov88@gmail.com>
2026-01-22 12:42:30 -0700

    2config.lua

---
+++ config.lua
@@ -0,0 +1,29 @@
+Config = {
+    Team = "Pirates",
+    Configuration = {
+        HopWhenIdle = true,
+        AutoHop = true,
+        AutoHopDelay = 60 * 60,
+        FpsBoost = false,
+        blackscreen = true
+    },
+    Items = {
+        AutoFullyMelees = true,
+        Saber = true,
+        CursedDualKatana = false,
+        SoulGuitar = false,
+        RaceV2 = true
+    },
+    Settings = {
+        StayInSea2UntilHaveDarkFragments = false
+    }
+}
+```
+
+4. Click **Commit new file** (green button at bottom)
+5. Add a message like "Initial config" and commit
+
+**Step 3: Get your config URL**
+Your config file URL will be:
+```
+https://raw.githubusercontent.com/YOUR_USERNAME/game-config/main/config.lua
